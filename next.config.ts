fc1dcb17768289b68df19d7a66a82ae2d9ec2350
sssad-a-little-bit <kdisksdkldlw@proton.me>
2026-09-23 17:19:53 +0300

    Configure GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/da-vinci-site-118",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
